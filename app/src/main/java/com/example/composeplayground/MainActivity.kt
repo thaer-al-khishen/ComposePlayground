fa7ae7b6cf97b0ddf50537b425536d033c6c9726
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2023-11-25 13:57:33 +0200

    Create Rows

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,25 +3,35 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column_SpaceBetween_fill_height_scrollable()
-//            ScrollableColumn(
-//                modifier = Modifier.fillMaxSize(),
-//                horizontalAlignment = Alignment.Start
-//            ) {
-//                for (item in 1..50) {
-//                    Text(text = "Column item: $item")
-//                }
-//            }
+            ScrollableRow(
+                modifier = Modifier.background(Color.Cyan).fillMaxSize()
+            ) {
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+                Text(text = "Android")
+            }
         }
     }
 }
